1f0b5e35dd62ca58c0a6c4865383dc835904731f
peaceman <nixalio@gmail.com>
2021-05-05 16:48:58 +0200

    Use the object variant of database configuration to allow setting further options

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,13 @@ const config = require('config');
 
 module.exports = {
   client: 'mysql2',
-  connection: config.get('database.connectionUrl'),
+  connection: {
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    user: config.get('database.user'),
+    password: config.get('database.password'),
+    database: config.get('database.name'),
+    supportBigNumbers: true,
+    bigNumberStrings: true,
+  },
 };
